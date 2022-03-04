1e23c54f900b80c43b9cfa089fe9998280d2e64a
ElhamNawabi <nawabi.elham@gmail.com>
2022-03-03 23:12:10 -0500

    setting up hero

---
+++ src/components/Hero.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import condo from '../assets/images/condo.jpg';
+import villas from '../assets/images/villas.jpg';
+import beach from '../assets/images/beach.jpg';
+import hotels from '../assets/images/hotels.jpg';
+
+function Hero () {
+  return (
+    <section id="hero-section">
+    <div className="container">
+        <h1>
+            Your Reserved Relaxation Spot Just A Few Steps Away
+        </h1>
+    </div>
+    </section>
+  );
+};
+
+export default Hero
