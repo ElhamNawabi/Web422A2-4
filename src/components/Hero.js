ebb8df1436f07fe2c828b68d9c39fdc826a8231d
ElhamNawabi <nawabi.elham@gmail.com>
2022-03-04 14:30:56 -0500

    Hero tinkering

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,18 +1,74 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import { Carousel } from "react-bootstrap";
 import condo from '../assets/images/condo.jpg';
 import villas from '../assets/images/villas.jpg';
 import beach from '../assets/images/beach.jpg';
 import hotels from '../assets/images/hotels.jpg';
 
+
 function Hero () {
+  const [index, setIndex] = useState(0);
+
+  const handleSelect = (selectedIndex, e) => {
+    setIndex(selectedIndex);
+  };
+
   return (
-    <section id="hero-section">
-    <div className="container">
-        <h1>
-            Your Reserved Relaxation Spot Just A Few Steps Away
-        </h1>
+    <div className="hero">
+    <Carousel activeIndex={index} onSelect={handleSelect}>
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src={condo}
+          alt="First slide"
+        />
+        <Carousel.Caption>
+          <h3>First slide label</h3>
+          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src={villas}
+          alt="Second slide"
+        />
+
+        <Carousel.Caption>
+          <h3>Second slide label</h3>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src={beach}
+          alt="Third slide"
+        />
+
+        <Carousel.Caption>
+          <h3>Third slide label</h3>
+          <p>
+            Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+          </p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src={hotels}
+          alt="Fourth slide"
+        />
+
+        <Carousel.Caption>
+          <h3>Fourth slide label</h3>
+          <p>
+            Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+          </p>
+        </Carousel.Caption>
+      </Carousel.Item>
+    </Carousel>
     </div>
-    </section>
   );
 };
 //test
